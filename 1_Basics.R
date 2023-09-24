7961f0b37566518ff923fdc9e4346aec9f2b9d60
Luic Damian <luicdamian@gmail.com>
2023-09-24 16:51:19 +0200

    0.5

--- 1_Basics.R
+++ 1_Basics.R
@@ -1,3 +1,26 @@
 #### B2/ETX3 Tutorial ####
 
 ##### R Script 1: Basics (packages, loading data, basic data manipulation, plotting)
+
+
+###### Packages ######
+
+  # Packages are used to import
+
+
+
+###### Loading data #####
+
+  # Set working directory
+  setwd("C:/Users/Luic/OneDrive/Master/Hiwi/Stats Tutorial 23_24/Data")
+
+  data = read.csv("owid-co2-data.csv")
+
+  
+  ger = data[data$country == "Germany",]
+
+  plot(ger$year, ger$co2)  
+  plot(ger$year, ger$cement_co2_per_capita)
+  plot(ger$year, ger$cement_co2/ger$population)
+  
+  
